1206b0d9113c8e7d8f526728e22d80587e6c5694
Cfnatic7 <78828448+Cfnatic7@users.noreply.github.com>
2023-04-18 01:21:03 +0200

    feat: added city list

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,45 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText cityEditText;
+    private Button addCityButton;
+    private ListView cityListView;
+
+    private ArrayList<String> cityList;
+    private ArrayAdapter<String> cityListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cityEditText = findViewById(R.id.cityEditText);
+        addCityButton = findViewById(R.id.addCityButton);
+        cityListView = findViewById(R.id.cityListView);
+
+        cityList = new ArrayList<>();
+        cityListAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, cityList);
+        cityListView.setAdapter(cityListAdapter);
+
+        addCityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String cityName = cityEditText.getText().toString();
+                if (!cityName.isEmpty()) {
+                    cityList.add(cityName);
+                    cityListAdapter.notifyDataSetChanged();
+                    cityEditText.getText().clear();
+                }
+            }
+        });
     }
 }
